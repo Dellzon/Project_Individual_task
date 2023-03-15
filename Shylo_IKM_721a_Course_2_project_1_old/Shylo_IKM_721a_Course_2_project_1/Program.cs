fa01c701b53d6f6ab339258ceaa3868e93476d43
Dellzon <bigdady130999@gmail.com>
2023-03-15 14:11:14 +0200

    задание2

---
+++ Shylo_IKM_721a_Course_2_project_1_old/Shylo_IKM_721a_Course_2_project_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shylo_IKM_721a_Course_2_project_1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
